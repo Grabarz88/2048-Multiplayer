9cd69caf28295844e57f8c7b7820d491a06af75b
Grabarz88 <pawel.otto1999@o2.pl>
2022-08-22 18:00:08 +0200

    Prototyping the basic mechanics
    For now. The "2" block" can make a single move to left or right.

---
+++ Assets/FieldScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldScript : MonoBehaviour
+{
+
+    [SerializeField] int TableNumberX;
+    [SerializeField] int TableNumberY;
+
+    [SerializeField] float positionX;
+    [SerializeField] float positionY;
+
+    [SerializeField] bool isTaken;
+
+
+
+}
