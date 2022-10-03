006d979242f15936503ea0bbbf9fd6cb76157709
Grabarz88 <pawel.otto1999@o2.pl>
2022-10-03 16:11:56 +0200

    #9 Camera centering properly

--- Assets/Scripts/CameraPlacement.cs
+++ Assets/Scripts/CameraPlacement.cs
@@ -7,13 +7,26 @@ public class CameraPlacement : MonoBehaviour
     
     [SerializeField] GameObject FieldSpawner;
     [SerializeField] SpawnField SpawnField;
+    [SerializeField] GameObject CustomSetter;
 
 
 
     void Start()
     {
-        SpawnField = FieldSpawner.GetComponent<SpawnField>();
-        transform.position = new Vector3 (SpawnField.PodajSzerokoscPlanszy*16, SpawnField.PodajWysokoscPlanszy*16, -10);        
+        if(GameObject.Find("CustomSetter"))
+        {
+            CustomSetter = GameObject.Find("CustomSetter");
+            transform.position = new Vector3 (CustomSetter.GetComponent<CustomSetterScript>().X*16, CustomSetter.GetComponent<CustomSetterScript>().Y*16, -10);
+            
+        }
+        else
+        {
+            SpawnField = FieldSpawner.GetComponent<SpawnField>();
+            transform.position = new Vector3 (SpawnField.PodajSzerokoscPlanszy*16, SpawnField.PodajWysokoscPlanszy*16, -10);
+        }
+        
+        
+                
     }
 
   
